d471b6ba77804631bfc720ffd6b2f7b6061339bf
RyleeSKS <30046076@tafe.wa.edu.au>
2022-03-21 14:53:49 +0800

    2:53pm 21/03.2022 added delegate

--- AT3IndieGame/Assets/scripts/testInteractionToo.cs
+++ AT3IndieGame/Assets/scripts/testInteractionToo.cs
@@ -6,11 +6,20 @@ public class testInteractionToo : MonoBehaviour, IInteractable
 {
     public testInteration interaction;
 
+    private void Start()
+    {
+        interaction.interactionDeleagte += TestMethodThree;
+    }
+
     public void Activate()
     {
         Debug.Log("the interaction is currently turned on:" + interaction.ExampleBool);
         interaction.Activate();
     }
 
+    private void TestMethodThree()
+    {
+        Debug.Log("test method 3 has been activated");
+    }
     
 }
--- AT3IndieGame/Assets/scripts/testInteration.cs
+++ AT3IndieGame/Assets/scripts/testInteration.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 //RANDOM NOTE: in blackboard go to 'Unity tutorials' and 'programing in C# with Unity' find 'unityC#programming.pdf
 public class testInteration : MonoBehaviour, IInteractable
 {
+    #region boolean definistion
     private bool exampleBool;
 
     public bool ExampleBool //{ get; private set; } (another option)
@@ -11,9 +12,36 @@ public class testInteration : MonoBehaviour, IInteractable
         get { return exampleBool; } //how to make a privately set varriable to be publically read
         //set { exampleBool = value; } (how to make a setter
     }
+    #endregion
 
+    public delegate void InteractionDeleagte();
+    // you can use events too (they work similarly to Delegate)
+    public InteractionDeleagte interactionDeleagte;
+
+    private void OnEnable()
+    {
+        interactionDeleagte = new InteractionDeleagte(TestMethod);
+        interactionDeleagte += TestMethodtwo;
+    }
+
+    private void OnDisable()
+    {
+        interactionDeleagte -= TestMethod;
+        interactionDeleagte -= TestMethodtwo;
+    }
+
+
+    /*
+    private void Start()
+    {
+        interactionDeleagte = new InteractionDeleagte(TestMethod);
+        interactionDeleagte += TestMethodtwo; //how to call two methods within the same delgate
+    }
+    */
     public void Activate()
     {
+        #region examples
+        /*
         //ExampleBool = true; (how to make a setter)
         exampleBool = !exampleBool;
         if(exampleBool == true)
@@ -24,6 +52,18 @@ public class testInteration : MonoBehaviour, IInteractable
         {
             Debug.Log("Case z.");
         }
-        
+        */
+        #endregion
+
+        interactionDeleagte.Invoke();
+    }
+
+    private void TestMethod()
+    {
+        Debug.Log("first method has been exactued");
+    }
+    private void TestMethodtwo()
+    {
+        Debug.Log("second method has been exactued");
     }
 }
